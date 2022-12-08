d42ca162812f661710079a9fe1f7fc039f2f4bca
Kumar Gaurav <kmrgaurav33@gmail.com>
2022-12-08 16:59:50 +0530

    feat: commiting compiled code for distribution

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.add = void 0;
+var add = function (a, b) {
+    return a + b;
+};
+exports.add = add;
+exports["default"] = {
+    add: exports.add
+};
+//# sourceMappingURL=index.js.map
